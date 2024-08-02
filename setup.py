06b9fb0a95efebeb6901a91080eefcf4658e2579
Adam Koziol <adam.koziol@canada.ca>
2024-08-02 14:40:46 -0400

    chore: add setup.py

--- setup.py
+++ setup.py
@@ -1,63 +1,35 @@
-from cx_Freeze import setup, Executable
-from pathlib import Path
+"""
+Setup script for the PoreSippr package.
+"""
+
+from setuptools import setup
 
 # Import the version from version.py
 from version import __version__
 
-def collect_files_and_folders(directory, base_dir):
-    """
-    Collect all files and folders in the given directory and return their
-    absolute and relative paths.
+# Read dependencies from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-    :param directory: The directory to traverse.
-    :param base_dir: The base directory to calculate relative paths.
-    :return: A list of tuples containing absolute and relative paths.
-    """
-    collected_items = []
-    base_path = Path(base_dir).resolve()
-    for path in Path(directory).rglob('*'):
-        norm_path = path.resolve()
-        rel_path = norm_path.relative_to(base_path)
-        if norm_path.is_file():
-            collected_items.append((str(norm_path), str(rel_path)))
-    return collected_items
-
-# Collect files and folders from specified directories
-config_files = collect_files_and_folders('config', 'config')
-fonts_files = collect_files_and_folders('fonts', 'fonts')
-icons_files = collect_files_and_folders('icons', 'icons')
-venv_files = collect_files_and_folders('/home/adamkoziol/miniconda/envs/poresippr_gui', '/home/adamkoziol/miniconda/envs/poresippr_gui')
-
-# Define build options for cx_Freeze
-build_options = {
-    'packages': [],
-    'excludes': ['importlib_resources'],
-    'include_files': [
-        ('cfia.jpg', '.'),
-        ('CFIA_logo.png', '.'),
-        ('methods.py', '.'),
-        ('poresippr_basecall_scheduler.py', '.'),
-        ('poresippr_placeholder.py', '.'),
-        ('report_template.html', '.'),
-        ('ui_main.py', '.'),
-        ('version.py', '.'),
-        *config_files,
-        *fonts_files,
-        *icons_files,
-        *venv_files,
-    ],
-}
-
-# Define executables for cx_Freeze
-executables = [
-    Executable('main.py', base=None, target_name='PoreSippr', icon='cfia.jpg')
-]
-
-# Setup the build using cx_Freeze
+# Set up the package
 setup(
     name='PoreSippr',
     version=__version__,
     description='PoreSippr Application',
-    options={'build_exe': build_options},
-    executables=executables
+    long_description='PoreSippr is a GUI application for processing and "'
+                     '"analyzing MinION sequencing data in real-time.',
+    author='Adam Koziol',
+    author_email='adam.koziol@inspection.gc.ca',
+    url='https://github.com/OLC-Bioinformatics/PoreSippr',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    packages=['poresippr'],
+    include_package_data=True,
+    install_requires=requirements,
 )
